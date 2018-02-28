851a6d5de7e29de1f7ba8c7f5744ed1c333fc262
David Morrill <judimorrill@Judis-MacBook-Pro.local>
2018-02-28 01:55:35 -0800

    update photo path

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,32 +1,32 @@
 //create array of default friends
 
 var friends = [{
-    name: "John Torpey",
-    photo: "https://static.pexels.com/photos/343717/pexels-photo-343717.jpeg",
+    name: "Mila Kunis",
+    photo: "FriendFinder/app/public/images/milakunis.jpg",
     scores: [3, 1, 4, 4, 2, 4, 5, 2, 4, 2]
 }, {
-    name: "Laura Toner",
-    photo: "https://static.pexels.com/photos/206481/pexels-photo-206481.jpeg",
+    name: "George Clooney",
+        photo: "FriendFinder/app/public/images/george_clooney.jpg",
     scores: [2, 5, 3, 4, 5, 3, 1, 3, 2, 4]
 }, {
-    name: "Bob Merad",
-    photo: "https://static.pexels.com/photos/249760/pexels-photo-249760.jpeg",
+    name: "Kate Upton",
+    photo: "FriendFinder/app/public/images/kate_upton.jpeg",
     scores: [4, 3, 3, 2, 1, 2, 5, 4, 2, 1]
 }, {
-    name: "Amy Crosby",
-    photo: "https://static.pexels.com/photos/270308/pexels-photo-270308.jpeg",
+    name: "Kevin Hart",
+    photo: "FriendFinder/app/public/images/kevin_hart.jpg",
     scores: [1, 2, 3, 3, 4, 1, 2, 5, 3, 3]
 }, {
-    name: "Chris Maclin",
-    photo: "https://static.pexels.com/photos/249757/pexels-photo-249757.jpeg",
+    name: "Tom Brady",
+    photo: "FriendFinder/app/public/images/tom_brady.jpeg",
     scores: [5, 4, 5, 3, 2, 4, 2, 1, 3, 2]
 }, {
-    name: "Anna Schultz",
-    photo: "https://static.pexels.com/photos/388517/pexels-photo-388517.jpeg",
+    name: "Elle McPherson",
+    photo: "FriendFinder/app/public/images/elle_macpherson.jpeg",
     scores: [2, 1, 1, 2, 5, 4, 3, 3, 1, 4]
 }, {
-    name: "Jack Waller",
-    photo: "https://static.pexels.com/photos/64385/pexels-photo-64385.jpeg",
+    name: "Blake Lively",
+    photo: "FriendFinder/app/public/images/blake-lively.png",
     scores: [2, 2, 1, 3, 4, 2, 5, 3, 3, 4]
 }];
 
