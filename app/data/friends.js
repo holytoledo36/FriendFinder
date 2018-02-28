ee943628fbd4cd403b76ea6b59b64250546b072a
David Morrill <judimorrill@airbears2-10-142-117-237.airbears2.1918.berkeley.edu>
2018-02-28 09:52:35 -0800

    fixed image path

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,31 +2,31 @@
 
 var friends = [{
     name: "Mila Kunis",
-    photo: "FriendFinder/app/public/images/milakunis.jpg",
+    photo: "/images/milakunis.jpg",
     scores: [3, 1, 4, 4, 2, 4, 5, 2, 4, 2]
 }, {
     name: "George Clooney",
-        photo: "FriendFinder/app/public/images/george_clooney.jpg",
+        photo: "/images/george_clooney.jpg",
     scores: [2, 5, 3, 4, 5, 3, 1, 3, 2, 4]
 }, {
     name: "Kate Upton",
-    photo: "FriendFinder/app/public/images/kate_upton.jpeg",
+    photo: "/images/kate_upton.jpeg",
     scores: [4, 3, 3, 2, 1, 2, 5, 4, 2, 1]
 }, {
     name: "Kevin Hart",
-    photo: "FriendFinder/app/public/images/kevin_hart.jpg",
+    photo: "/images/kevin_hart.jpg",
     scores: [1, 2, 3, 3, 4, 1, 2, 5, 3, 3]
 }, {
     name: "Tom Brady",
-    photo: "FriendFinder/app/public/images/tom_brady.jpeg",
+    photo: "/images/tom_brady.jpeg",
     scores: [5, 4, 5, 3, 2, 4, 2, 1, 3, 2]
 }, {
     name: "Elle McPherson",
-    photo: "FriendFinder/app/public/images/elle_macpherson.jpeg",
+    photo: "/images/elle_macpherson.jpeg",
     scores: [2, 1, 1, 2, 5, 4, 3, 3, 1, 4]
 }, {
     name: "Blake Lively",
-    photo: "FriendFinder/app/public/images/blake-lively.png",
+    photo: "/images/blake-lively.png",
     scores: [2, 2, 1, 3, 4, 2, 5, 3, 3, 4]
 }];
 
